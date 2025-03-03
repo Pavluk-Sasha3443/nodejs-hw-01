be491f63f416809baf79093d34228aedc704bd8a
Sasha Pavluk <bogota3443@gmail.com>
2025-03-03 14:28:39 +0200

    commit

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await readContacts();
+    return data;
+  } catch (error) {
+    console.error('Error reading file:', error);
+  }
+};
 
 console.log(await getAllContacts());
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await readFile(PATH_DB, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading file:', error);
+  }
+};
